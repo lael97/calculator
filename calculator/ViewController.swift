0d361c04f3c85dac3c658c1e22fdfca885590887
Alexis Lael Gonzalez Garcia <alexis@pixan.io>
2019-01-26 19:07:26 -0800

    first commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get the bill amount        
+        let bill = Double(billField.text!) ?? 0
+        //Calculate the tip and the total
+        let tip = bill * 0.1
+        let total = bill + tip
+        //Update the tip and total labels
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
 }
 
